fef7971add24e6b681c75c5bdad84c10359d2c6d
Jennie R <jbiakcin@icloud.com>
2020-03-02 11:27:35 -0800

    Added backend controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users
-    resource :session
+    resource :session, only: [:create, :destroy]
   end
 
   root "static_pages#root"
