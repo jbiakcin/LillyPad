dec78885682624ccdffb1e4d18e762b536494c85
Jennie R <jbiakcin@icloud.com>
2020-05-15 13:30:59 -0700

    Add reviews reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,12 +4,14 @@ import UsersReducer from './users_reducer';
 import SpotsReducer from './spots_reducer';
 import BookingsReducer from './bookings_reducer';
 import SearchReducer from './search-reducer';
+import ReviewsReducer from './reviews_reducer';
 
 const entitiesReducer = combineReducers ({
   users: UsersReducer,
   spots: SpotsReducer,
   bookings: BookingsReducer,
-  search: SearchReducer
+  search: SearchReducer,
+  reviews: ReviewsReducer
 });
 
 export default entitiesReducer;
--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -4,3 +4,24 @@ import {
   REMOVE_REVIEW
 } from '../actions/review_actions';
 import {RECEIVE_SPOTS} from '../actions/spot_actions';
+import { RECEIVE_BOOKINGS, REMOVE_BOOKING } from '../actions/booking_actions';
+
+const ReviewsReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_SPOTS:
+    case RECEIVE_BOOKINGS:
+      return Object.assign({}, state, action.reviews);
+    case RECEIVE_BOOKING:
+      return Object.assign({}, state, {[action.review.id]: action.review});
+    case REMOVE_BOOKING:
+      let newState = Object.assign({}, state);
+      delete newState[action.reviewId];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default ReviewsReducer;
