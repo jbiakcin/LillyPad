f694fa2936c5bf1e99f7a5a83cd4c64a3e24091e
Jennie R <jbiakcin@icloud.com>
2020-05-17 23:47:56 -0700

    Re-seed database

--- frontend/components/review/review_index.jsx
+++ frontend/components/review/review_index.jsx
@@ -14,6 +14,7 @@ class ReviewIndex extends React.Component {
     let allUsers = this.props.allUsers;
     let updateReview = this.props.updateReview;
     let currentUserId = this.props.currentUserId;
+    debugger;
     let spotReviews = allReviews.filter(review => review.spot_id === spot.id);
     
     let reviews;
